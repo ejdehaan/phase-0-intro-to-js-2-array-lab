89a5f8c5ad084249c93594a01cdea6b1bb784660
Emily Joy DeHaan <emilydehaan001@gmail.com>
2022-03-27 20:24:18 -0400

    Passed all but one test

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
-// Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"]
+
+
+function appendCat(name) {
+    const copyOfCats = [
+        ...cats,
+        name,]
+        return copyOfCats
+}
+
+
+function prependCat(name) {
+    const copyOfCats2 = [
+        name,
+        ...cats,]
+        return copyOfCats2
+}
+prependCat("Arnold")
+
+
+function removeLastCat() {
+    const copyOfCats3 = cats.slice(0,2)
+    return copyOfCats3
+}
+
+function removeFirstCat() {
+    const copyOfCats4 = cats.slice(1,3)
+    return copyOfCats4
+}
+
+
+function destructivelyAppendCat(name) {
+    cats.push(name)
+}
+
+
+destructivelyAppendCat("Ralph")
+//console.log(cats);
+
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+}
+
+
+destructivelyPrependCat("Bob")
+//console.log(cats);
+
+
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+//console.log(cats);
+
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+
